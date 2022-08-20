970e9ab46f4e1cec9991e3955aeaf40b992abfea
noorzeyad <110266242+noorzeyad@users.noreply.github.com>
2022-08-20 10:09:48 +0300

    cw6 is done
    done

---
+++ script.js
@@ -0,0 +1,20 @@
+alert("مرحباً بك في موقعي الذي سيحسب درجتك!!");
+
+ let grade = prompt("اكتب درجتك بالأرقام");
+
+ console.log(grade);
+
+if (grade >= 90 && grade <=100) {
+    console.log("لقد حصلت على امتياز 🥳");
+}  else if(grade >= 80){
+   console.log("لقد حصلت على جيد جداً🤩");
+} else if(grade >= 70){
+    console.log("لقد حصلت على جيد🙂");
+} else if(grade >= 60){
+    console.log("لقد حصلت على مقبول😕");
+} else if (grade >= 50){
+    console.log("لقد حصلت على ضعيف☹️");
+} else {
+    console.log("راسب💔");
+}
+
